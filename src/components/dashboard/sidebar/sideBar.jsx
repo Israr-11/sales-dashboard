a9d461a0b6a43eafc6cbcf3fac3ea109deaa881e
Israr Ahmed <israrahmedpk1999@gmail.com>
2024-03-10 18:22:48 +0500

    fourth commit

--- src/components/dashboard/sidebar/sideBar.jsx
+++ src/components/dashboard/sidebar/sideBar.jsx
@@ -2,41 +2,54 @@ import React from "react";
 import "../../../assets/styles/sideBar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import IconSet from "../../common/IconSet";
-import { Nav,ListGroup } from "react-bootstrap";
+import { Nav, ListGroup } from "react-bootstrap";
 
-const SideBar =()=>{
-    return(
-        <Nav className="flex-column">
+const SideBar = () => {
+    return (
+        <Nav className="flex-column m-5">
 
-       <ListGroup horizontal style={{border:"none", padding:"0"}}>
-      <ListGroup.Item><FontAwesomeIcon icon={IconSet.dashboard}/></ListGroup.Item>
-      <ListGroup.Item>  <p>Dashboard</p></ListGroup.Item>
-      </ListGroup>
- 
+            <ListGroup horizontal>
+                <ListGroup.Item><FontAwesomeIcon icon={IconSet.dashboard} /></ListGroup.Item>
+                <ListGroup.Item>  <p>Dashboard</p></ListGroup.Item>
+            </ListGroup>
 
-      
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.leaderboard} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Leaderboard</p> </ListGroup.Item>
+            </ListGroup>
 
-        <FontAwesomeIcon icon={IconSet.leaderboard} />            
-            <p>Leaderboard</p>
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.order} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Order</p> </ListGroup.Item>
+            </ListGroup>
 
-        <FontAwesomeIcon icon={IconSet.order}/>
-             <p>Order</p>
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.products} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Products</p> </ListGroup.Item>
+            </ListGroup>
 
-        <FontAwesomeIcon icon={IconSet.products}/>
-        <p>Products</p>
 
-        <FontAwesomeIcon icon={IconSet.salesReport}/>
-        <p>Sales Report</p>
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.salesReport} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Sales Report</p> </ListGroup.Item>
+            </ListGroup>
 
-      <FontAwesomeIcon icon={IconSet.messages}/>
-        <p>Messages</p>
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.messages} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Messages</p> </ListGroup.Item>
+            </ListGroup>
 
-       <FontAwesomeIcon icon={IconSet.settings}/>
-        <p>Setting</p>
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.settings} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Setting</p> </ListGroup.Item>
+            </ListGroup>
 
-       <FontAwesomeIcon icon={IconSet.signout}/>
-        <p>Signout</p>
-        
+
+            <ListGroup horizontal>
+                <ListGroup.Item> <FontAwesomeIcon icon={IconSet.signout} /> </ListGroup.Item>
+                <ListGroup.Item> <p>Signout</p> </ListGroup.Item>
+            </ListGroup>
+
         </Nav>
     )
 }
